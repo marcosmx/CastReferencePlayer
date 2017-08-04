dfcc490a1bc279861b7f7d9d5cae81ee34737e90
Marco Garay <mgaray@ooyala.com>
2017-08-04 16:35:29 -0500

    test 4.1

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+cast.player.api.setLoggerLevel(cast.player.api.LoggerLevel.DEBUG);
+cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
+
+
+
 videoEl = document.getElementById('player');
 crm = cast.receiver.CastReceiverManager.getInstance();
 //mediaManager = new cast.receiver.MediaManager(videoEl)
