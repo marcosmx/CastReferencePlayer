8454263e3d873e90c3d74f6d50138ae0410266dd
Marco Garay <mgaray@ooyala.com>
2017-08-04 17:28:18 -0500

    test 4.3

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-
-cast.player.api.setLoggerLevel(cast.player.api.LoggerLevel.DEBUG);
-cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
-
-
+cast
+    .player
+    .api
+    .setLoggerLevel(cast.player.api.LoggerLevel.DEBUG);
+cast
+    .receiver
+    .logger
+    .setLevelValue(cast.receiver.LoggerLevel.DEBUG);
 
 videoEl = document.getElementById('player');
-crm = cast.receiver.CastReceiverManager.getInstance();
+crm = cast
+    .receiver
+    .CastReceiverManager
+    .getInstance();
 //mediaManager = new cast.receiver.MediaManager(videoEl)
 crm.start();
 
 crm.onReady = (event) => {
     let capabilities = crm.getDeviceCapabilities();
-    console.log("las caps ",capabilities);
+    console.log("las caps ", capabilities);
+}
+
+crm.onSenderConnected = function (event) {
+    senders = crm.getSenders();
+    printDebugMessage("connected senders", senders);
+}
+
+crm.onSenderDisconnected = function (event) {
+    senders = crm.getSenders();
+    // if the last sender disconnects, then stop the cast session entirely if it was
+    // an explicit disconnection
+    if ((senders.length === 0) && (event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER)) {
+        crm.stop();
+    }
+}
+
+crm.onShutdown = function (event) {
+    senders = crm.getSenders();
+    console.log(senders);
 }
