fbe04caea970469722181b419bfeb201d909bc5b
Marco Garay <mgaray@ooyala.com>
2017-09-07 18:50:17 -0500

    first try using ios app

--- app.js
+++ app.js
@@ -10,7 +10,30 @@ mediaManager = new cast.receiver.MediaManager(element_);
 ////media Manager stuff
 
 mediaManager.onLoad = function(event){
-    console.log(event);
+    let data = event.data.media.customData;
+    // TODO: Handle loading Screen
+    
+    initPlayer(data);
+    mediaManager.sendStatus(event.senderId, event.data.requestId, true);
+}
+
+function initPlayer(data){
+    let params = {
+        'autoplay': true,
+        'loop': false
+    };
+
+    if (data.params["embedToken"] !== undefined) {
+        params["embedToken"] = data.params["embedToken"];
+    }
+
+    OO.ready(function(){
+        window.pp = OO.Player.create(
+            'video', // element id
+            data.ec, // Embed code
+            params
+        )
+    });
 }
 
 
