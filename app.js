3468f2ef16fc4e808fe913c46f3366983649edc8
Marco Garay <mgaray@ooyala.com>
2017-09-13 18:34:04 -0500

    fix query template selector

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //elements block
 
 var _splashStatus = document.querySelector('#status-cast');
-var _playerEl = document.querySelector('#temp-video');
+var _playerEl = document.getElementById('temp-video');
 
 const _eventnamespace = "ooyala-chromecast";
 const _messagebusnamespace = "urn:x-cast:ooyala";
@@ -19,7 +19,7 @@ var playerCtrl = (function (OO) {
     //utils
 
     function _getVideoEl(elementId) {
-        var el = document.querySelector(`${elementId} video`) || document.querySelector(`${elementId}`)
+        var el = document.querySelector(`${elementId} video`) || document.querySelector(`${elementId}`);
         if (el && el.nodeName !== "VIDEO") {
             throw `Video Element with ID: {elementId} not found`
         }
@@ -30,11 +30,15 @@ var playerCtrl = (function (OO) {
     // Player events handlers
 
     function _onPlayerCreated(e, data) {
+
         // here we need to handle the player ui controls
         console.log("on player Created", e, data, arguments);
+    }
+
+    function _onVcCreatedElement(e, data) {
+        console.log("VC created Element");
         _playerEl.remove();
         _playerEl = _getVideoEl(data.domId);
-        console.log("playerEL: ", _playerEl);
         if (_playerEl) {
             mediaManager.setMediaElement(_playerEl);
         }
@@ -42,7 +46,7 @@ var playerCtrl = (function (OO) {
 
     function _onPlayheadTimeChanged() {
         _playHeadInfo = [...arguments];
-        //console.log("playhead time changed: ", _playHeadInfo);
+        console.log("playhead time changed: ", _playHeadInfo);
     }
 
     function _onInitialPlay(e){
@@ -64,7 +68,7 @@ var playerCtrl = (function (OO) {
     }
 
     function _onCreate(player) {
-        //player.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_CREATED, _eventnamespace, _onVcCreatedElement);
+        player.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_CREATED, _eventnamespace, _onVcCreatedElement);
         player.mb.subscribe(OO.EVENTS.PLAYER_CREATED, _eventnamespace, _onPlayerCreated);
         player.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, _eventnamespace, _onPlayheadTimeChanged);
         player.mb.subscribe(OO.EVENTS.INITIAL_PLAY, _eventnamespace, _onInitialPlay);
