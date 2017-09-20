3065f06d888f17bd2a256f80d9e09bdfb6b61053
Marco Garay <mgaray@ooyala.com>
2017-09-19 23:13:37 -0500

    refactor with the correct event for play

--- app.js
+++ app.js
@@ -12,194 +12,92 @@ var _player = null;
 var _playHeadInfo = {};
 var _currentAsset = null;
 
-// ooyala player controller
+// ooyala player controller utils
 
-    //utils
-
-    function _getVideoEl(elementId) {
-        var el = document.querySelector(`#${elementId} video`) || document.querySelector(`#${elementId}`);
-        if (el && el.nodeName !== "VIDEO") {
-            throw `Video Element with ID: {elementId} not found`
-        }
-
-        return el;
+function _getVideoEl(elementId) {
+    var el = document.querySelector(`#${elementId} video`) || document.querySelector(`#${elementId}`);
+    if (el && el.nodeName !== "VIDEO") {
+        throw `Video Element with ID: ${elementId} not found`
     }
 
-    // Player events handlers
+    return el;
+}
 
-    function _onPlayerCreated(e, data) {
+// Player events handlers
 
-        // here we need to handle the player ui controls
-        console.log("on player Created", e, data, arguments);
+function _onPlayerCreated(e, data) {
+    // here we need to handle the player ui controls
+    console.log("on player Created", e, data, arguments);
+}
+
+function _onVcCreatedElement(e, data) {
+    console.log("VC created Element");
+    _playerEl.remove();
+    _playerEl = null;
+    _playerEl = _getVideoEl(data.domId);
+    if (_playerEl) {
+        mediaManager.setMediaElement(_playerEl);
     }
+}
 
-    function _onVcCreatedElement(e, data) {
-        console.log("VC created Element");
-        _playerEl.remove();
-        _playerEl = null;
-        _playerEl = _getVideoEl(data.domId);
-        if (_playerEl) {
-            mediaManager.setMediaElement(_playerEl);
-        }
-    }
+function _onPlayheadTimeChanged() {
+    _playHeadInfo = [...arguments];
+    //console.log("playhead time changed: ", _playHeadInfo);
+}
 
-    function _onPlayheadTimeChanged() {
-        _playHeadInfo = [...arguments];
-        //console.log("playhead time changed: ", _playHeadInfo);
-    }
+function _onPlaying(e) {
+    console.log("player ctrl: init playback ", e);
+    _messageBus.broadcast(JSON.stringify({0: "playing"}));
+}
 
-    function _onInitialPlay(e){
-        console.log("player ctrl: init playback ", e);
-        _messageBus.broadcast(JSON.stringify({0:"playing"}));
-    }
+function _onPause() {
+    _player
+        .mb
+        .publish(OO.EVENTS.PAUSE);
+}
 
-    function _onPause() {
-        _player.mb.publish(OO.EVENTS.PAUSE);
-    }
+function _onPaused(e) {
+    var message = Object.assign({}, e); // flatten the object and just keep direct properties
+    _messageBus.broadcast(JSON.stringify(message));
+}
 
-    function _onPaused(e) {
-        var message = Object.assign({}, e); // flatten the object and just keep direct properties
-        _messageBus.broadcast(JSON.stringify(message));
-    }
+function _onStop() {
+    _player
+        .mb
+        .publish(OO.EVENTS.PLAYED, {type: "stop"});
+}
 
-    function _onStop(){
-        _player.mb.publish(OO.EVENTS.PLAYED, {type:"stop"});
-    }
+function _onCreate(player) {
+    player.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_CREATED, _eventnamespace, _onVcCreatedElement);
+    player.mb.subscribe(OO.EVENTS.PLAYER_CREATED, _eventnamespace, _onPlayerCreated);
+    player.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, _eventnamespace, _onPlayheadTimeChanged);
+    player.mb.subscribe(OO.EVENTS.PAUSED, _eventnamespace, _onPaused);
+    player.mb.subscribe(OO.EVENTS.PLAYING, _eventnamespace, _onPlaying);
+}
 
-    function _onCreate(player) {
-        //player.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_CREATED, _eventnamespace, _onVcCreatedElement);
-        //player.mb.subscribe(OO.EVENTS.PLAYER_CREATED, _eventnamespace, _onPlayerCreated);
-        //player.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, _eventnamespace, _onPlayheadTimeChanged);
-        //player.mb.subscribe(OO.EVENTS.INITIAL_PLAY, _eventnamespace, _onInitialPlay);
-        //player.mb.subscribe(OO.EVENTS.PAUSED, _eventnamespace, _onPaused);
-        player.mb.subscribe(OO.EVENTS.PLAYING, "foo", function(e){
-            console.log(" video is playing, ", e, arguments);
+function initPlayer(data) {
+    var params = Object.assign({}, data.params);
+    params.onCreate = _onCreate;
+    _currentAsset = data.ec;
+    if (_player === null) {
+        console.log("player ctrl: about to create a new player instance", params);
+        OO.ready(function () {
+            _player = OO
+                .Player
+                .create('player', data.ec, params);
         });
-
-        player.mb.subscribe(OO.EVENTS.VC_PLAYING, "foo", function(e){
-            console.log(" video is playing on foo, ", e, arguments);
-        });
+    } else {
+        console.log("player ctrl: set new embed code: ", data.ec)
+        _player.setEmbedCode(data.ec, params);
     }
-
-    function initPlayer(data){
-        var params = Object.assign({}, data.params);
-        params.onCreate = _onCreate;
-        _currentAsset = data.ec;
-        if (_player === null) {
-            console.log("player ctrl: about to create a new player instance", params);
-            OO.ready(function () {
-                _player = OO.Player.create('player', data.ec, params);
-            });
-        } else{
-            console.log("player ctrl: set new embed code: ", data.ec)
-            _player.setEmbedCode(data.ec, params);
-        }        
-    }
-
-/*var playerCtrl = (function (OO) {
-    var _player = null;
-    var _playHeadInfo = {};
-    var _currentAsset = null;
-
-    //utils
-
-    function _getVideoEl(elementId) {
-        var el = document.querySelector(`#${elementId} video`) || document.querySelector(`#${elementId}`);
-        if (el && el.nodeName !== "VIDEO") {
-            throw `Video Element with ID: {elementId} not found`
-        }
-
-        return el;
-    }
-
-    // Player events handlers
-
-    function _onPlayerCreated(e, data) {
-
-        // here we need to handle the player ui controls
-        console.log("on player Created", e, data, arguments);
-    }
-
-    function _onVcCreatedElement(e, data) {
-        console.log("VC created Element");
-        _playerEl.remove();
-        _playerEl = null;
-        _playerEl = _getVideoEl(data.domId);
-        if (_playerEl) {
-            mediaManager.setMediaElement(_playerEl);
-        }
-    }
-
-    function _onPlayheadTimeChanged() {
-        _playHeadInfo = [...arguments];
-        //console.log("playhead time changed: ", _playHeadInfo);
-    }
-
-    function _onInitialPlay(e){
-        console.log("player ctrl: init playback ", e);
-        _messageBus.broadcast(JSON.stringify({0:"playing"}));
-    }
-
-    function _onPause() {
-        _player.mb.publish(OO.EVENTS.PAUSE);
-    }
-
-    function _onPaused(e) {
-        var message = Object.assign({}, e); // flatten the object and just keep direct properties
-        _messageBus.broadcast(JSON.stringify(message));
-    }
-
-    function _onStop(){
-        _player.mb.publish(OO.EVENTS.PLAYED, {type:"stop"});
-    }
-
-    function _onCreate(player) {
-        //player.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_CREATED, _eventnamespace, _onVcCreatedElement);
-        player.mb.subscribe(OO.EVENTS.PLAYER_CREATED, _eventnamespace, _onPlayerCreated);
-        player.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, _eventnamespace, _onPlayheadTimeChanged);
-        player.mb.subscribe(OO.EVENTS.INITIAL_PLAY, _eventnamespace, _onInitialPlay);
-        player.mb.subscribe(OO.EVENTS.PAUSED, _eventnamespace, _onPaused);       
-
-    }
-
-    function _initPlayer(data) {
-        var params = Object.assign({}, data.params);
-        params.onCreate = _onCreate;
-        //data.ec = "JpeWNhbjrfKs0sIelRvR8R3YIEATk1ZG";
-        //params.embedToken = "http://player.ooyala.com/sas/embed_token/lhNmYyOuUnCvRiHi5NbFBBLOG4xm/A2MGFyYTE6Dv_7o2E3zWqCcYAUyqO4t9?api_key=lhNmYyOuUnCvRiHi5NbFBBLOG4xm.S9VRE&expires=3042186829&&&&signature=AgkvXZW57DrxsLDdOiVEBWqkWPxxz75olqZfS505CeU";
-        //var extra = {"api_ssl_server":"https://player-staging.ooyala.com","api_server":"http://player-staging.ooyala.com","auth_ssl_server":"https://player-staging.ooyala.com/sas","auth_server":"http://player-staging.ooyala.com/sas"};
-        //Object.assign(params, extra);
-
-        _currentAsset = data.ec;
-        if (_player === null) {
-            console.log("player ctrl: about to create a new player instance", params);
-            OO.ready(function () {
-                _player = OO.Player.create('player', data.ec, params);
-            });
-        } else{
-            console.log("player ctrl: set new embed code: ", data.ec)
-            _player.setEmbedCode(data.ec, params);
-        }        
-    }
-
-    return {
-        setPlayer: _initPlayer,
-        getState: function() {
-            return _player.getState();
-        },
-        getPlayHead: ()=>(_playHeadInfo),
-        getCurrentAsset: ()=>(_currentAsset),
-        stop: _onStop,
-        pause: _onPause
-    };
-})(OO);*/
+}
 
 //set log level
 //cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
 
-_mediaManager = new cast.receiver.MediaManager(_playerEl);
 
 ////media Manager stuff
+_mediaManager = new cast.receiver.MediaManager(_playerEl);
 
 _mediaManager.onLoad = function(event){
     var data = event.data.media.customData;
@@ -214,11 +112,11 @@ _mediaManager.onLoad = function(event){
     _mediaManager.sendStatus(event.senderId, event.data.requestId, true);
 }
 
-/*_mediaManager["origOnStop"] = _mediaManager.onStop;
+_mediaManager["origOnStop"] = _mediaManager.onStop;
 _mediaManager.onStop = function(event) {
     playerCtrl.stop();
     _mediaManager["origOnStop"](event);
-}*/
+}
 
 _mediaManager["origOnPause"] = _mediaManager.onPause;
 _mediaManager.onPause = function(event) {
@@ -233,36 +131,8 @@ _mediaManager.customizedStatusCallback = function(ms) {
     return ms;
 }
 
-_castManager = cast.receiver.CastReceiverManager.getInstance();
-
-_messageBus = _castManager.getCastMessageBus(_messagebusnamespace);
-
-/* _messageBus.onMessage = function handleMessage(e) {
-    console.log(e);
-
-    var data = JSON.parse(e.data);
-    switch (data.action) {
-        case "setCCLanguage":
-            //setClosedCaptionsLanguage(message.data);
-            break;
-        case "getstatus":
-            var status = {
-                state: playerCtrl.getState(),
-                playhead: playerCtrl.getPlayHead(),
-                embed: playerCtrl.getCurrentAsset()
-            }
-            _messageBus.send(e.senderId, JSON.stringify(status));
-            break;
-        case "error":
-            //displayCastMediaError(message.message);
-            break;
-    }
-}; */
-
-
 // Cast Manager stuff
-
-
+_castManager = cast.receiver.CastReceiverManager.getInstance();
 
 _castManager.onReady = (event) => {
     console.log("System ready");
@@ -287,5 +157,30 @@ _castManager.onShutdown = function (event) {
     senders = _castManager.getSenders();
     console.log(senders);
 }
+
+// message Bus
+_messageBus = _castManager.getCastMessageBus(_messagebusnamespace);
+
+_messageBus.onMessage = function handleMessage(e) {
+    console.log(e);
+
+    var data = JSON.parse(e.data);
+    switch (data.action) {
+        case "setCCLanguage":
+            //setClosedCaptionsLanguage(message.data);
+            break;
+        case "getstatus":
+            var status = {
+                state: playerCtrl.getState(),
+                playhead: playerCtrl.getPlayHead(),
+                embed: playerCtrl.getCurrentAsset()
+            }
+            _messageBus.send(e.senderId, JSON.stringify(status));
+            break;
+        case "error":
+            //displayCastMediaError(message.message);
+            break;
+    }
+};
 
 _castManager.start();
