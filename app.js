c3b6a14ff9dae46305b046123b28c59967ae47db
Marco Garay <mgaray@ooyala.com>
2017-09-21 09:29:53 -0500

    refactor: test

--- app.js
+++ app.js
@@ -402,10 +402,10 @@ function printDebugMessage(command, event, ignorePattern) {
      ccLanguage = playerParams['ccLanguage'];
    }
 
-   //data.ec = "A2MGFyYTE6Dv_7o2E3zWqCcYAUyqO4t9";
-   //playerParams.embedToken = "http://player.ooyala.com/sas/embed_token/lhNmYyOuUnCvRiHi5NbFBBLOG4xm/A2MGFyYTE6Dv_7o2E3zWqCcYAUyqO4t9?api_key=lhNmYyOuUnCvRiHi5NbFBBLOG4xm.S9VRE&expires=3042186829&&&&signature=AgkvXZW57DrxsLDdOiVEBWqkWPxxz75olqZfS505CeU";
-   //var extra = {"api_ssl_server":"https://player-staging.ooyala.com","api_server":"http://player-staging.ooyala.com","auth_ssl_server":"https://player-staging.ooyala.com/sas","auth_server":"http://player-staging.ooyala.com/sas"};
-   //Object.assign(playerParams, extra);
+   data.ec = "A2MGFyYTE6Dv_7o2E3zWqCcYAUyqO4t9";
+   playerParams.embedToken = "http://player.ooyala.com/sas/embed_token/lhNmYyOuUnCvRiHi5NbFBBLOG4xm/A2MGFyYTE6Dv_7o2E3zWqCcYAUyqO4t9?api_key=lhNmYyOuUnCvRiHi5NbFBBLOG4xm.S9VRE&expires=3042186829&&&&signature=AgkvXZW57DrxsLDdOiVEBWqkWPxxz75olqZfS505CeU";
+   var extra = {"api_ssl_server":"https://player-staging.ooyala.com","api_server":"http://player-staging.ooyala.com","auth_ssl_server":"https://player-staging.ooyala.com/sas","auth_server":"http://player-staging.ooyala.com/sas"};
+   Object.assign(playerParams, extra);
 
    if (!playerParams.onCreate) {
      playerParams.onCreate = function(player) {
