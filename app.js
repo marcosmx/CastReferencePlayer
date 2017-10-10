e5cb184e6d867fff323a596665252fe5fc9486f9
Marco Garay <mgaray@ooyala.com>
2017-10-10 16:40:31 -0500

    test for ads

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  // constants
- var OOYALA_PLAYER_URL = "//player.ooyala.com/core/e48c9b51282d406f957c666149763424?plugins=bm";
+ var OOYALA_PLAYER_URL = "//player.ooyala.com/core/e48c9b51282d406f957c666149763424";
  var SPLASH_SCREEN_SRC = "./images/ooyala-logo.png";
  var LOGO_IMAGE_SRC = "./images/ooyala-logo.png";
  var PAUSE_ICON_SRC = " ./images/pause.svg";
@@ -404,9 +404,9 @@ function printDebugMessage(command, event, ignorePattern) {
 
    data.ec = "o2YWQ4YzE6ono3qI5c3fN-R3Jhi1yPXn";
    //data.ec = "hsdHIyYzE668escyHgrFiednk4831Un3"
-   playerParams.embedToken = "//player.ooyala.com/sas/embed_token/lhNmYyOuUnCvRiHi5NbFBBLOG4xm/o2YWQ4YzE6ono3qI5c3fN-R3Jhi1yPXn?api_key=lhNmYyOuUnCvRiHi5NbFBBLOG4xm.S9VRE&expires=1507108002&signature=U3zyP8bgBQwOZrofz8J+76rNtAVe4EkW5OMszkPTd1c&override_syndication_group=override_synd_groups_in_backlot";
-   var extra = {"api_ssl_server":"https://player-staging.ooyala.com","api_server":"http://player-staging.ooyala.com","auth_ssl_server":"https://player-staging.ooyala.com/sas","auth_server":"http://player-staging.ooyala.com/sas"};
-   Object.assign(playerParams, extra);
+   //playerParams.embedToken = "//player.ooyala.com/sas/embed_token/lhNmYyOuUnCvRiHi5NbFBBLOG4xm/o2YWQ4YzE6ono3qI5c3fN-R3Jhi1yPXn?api_key=lhNmYyOuUnCvRiHi5NbFBBLOG4xm.S9VRE&expires=1507108002&signature=U3zyP8bgBQwOZrofz8J+76rNtAVe4EkW5OMszkPTd1c&override_syndication_group=override_synd_groups_in_backlot";
+   //var extra = {"api_ssl_server":"https://player-staging.ooyala.com","api_server":"http://player-staging.ooyala.com","auth_ssl_server":"https://player-staging.ooyala.com/sas","auth_server":"http://player-staging.ooyala.com/sas"};
+   //Object.assign(playerParams, extra);
 
    if (!playerParams.onCreate) {
      playerParams.onCreate = function(player) {
